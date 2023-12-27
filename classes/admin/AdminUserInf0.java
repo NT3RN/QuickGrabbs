a676e49584a7e29de057661d17a0cd1e7876d4bf
NT3RN <98659883+NT3RN@users.noreply.github.com>
2023-12-27 17:56:16 +0600

    .

--- classes/admin/AdminUserInf0.java
+++ classes/admin/AdminUserInf0.java
@@ -14,7 +14,7 @@ import java.io.*;
 import interfaces.*;
 
 
-public class Adminuserinfo extends JFrame implements ActionListener {
+public class AdminUserInf0 extends JFrame implements ActionListener {
     JFrame w2f;
     private Container c;
     private Font f;
@@ -29,7 +29,7 @@ public class Adminuserinfo extends JFrame implements ActionListener {
     private String[] columns = {"Customer's Record"};
     private String[] rows = new String[3];
 
-    Adminuserinfo(){
+    AdminUserInf0(){
 
         w2f=new JFrame("User Information");
         w2f.setIconImage(new ImageIcon(getClass().getResource("/icons/framelogo.png")).getImage());
@@ -207,7 +207,7 @@ public class Adminuserinfo extends JFrame implements ActionListener {
                 pw.close();
                 JOptionPane.showMessageDialog(null, "New user account created successfully!","User created", JOptionPane.INFORMATION_MESSAGE);
 
-                new Adminuserinfo();
+                new AdminUserInf0();
                 w2f.setVisible(false);
             }catch(Exception e2){
                 System.out.println(e2);
